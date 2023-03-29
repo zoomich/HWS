44f191338235f81499b03bcad3f52cc70a54b7d9
Anton Kramarenko <zoomich.sqa@gmail.com>
2023-03-28 21:50:36 -0700

    custom modifiers work

--- ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -51,12 +51,95 @@ struct ContentView2: View {
                 useRedText.toggle()
                 }
                 .foregroundColor(useRedText ? .red : .blue)
+    }
 }
+
+struct CapsuleText: View {
+    var text: String
+
+    var body: some View {
+        Text(text)
+            .font(.largeTitle)
+            .padding()
+            .foregroundColor(.white)
+            .background(.blue)
+            .clipShape(Capsule())
+    }
 }
+
+
+struct ContentView3: View {
+    var body: some View {
+        VStack(spacing: 10) {
+            CapsuleText(text: "First")
+            CapsuleText(text: "Second")
+
+        }
+    }
+}
+
+//Custom modifiers
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.white)
+            .padding()
+            .background(.blue)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+}
+
+extension View {
+    func titleStyle() -> some View {
+        modifier(Title())
+    }
+}
+
+struct ContentView4: View {
+    var body: some View {
+        VStack(spacing: 10) {
+            Text("Hello World")
+                .modifier(Title())
+            Text("Hello World2")
+                .titleStyle()
+            Text("Hello World3")
+                 Color.blue
+                .frame(width: 300, height: 200)
+                .watermarked(with: "Hacking with Swift")
+
+        }
+    }
+}
+
+struct Watermark: ViewModifier {
+    var text: String
+
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+            content
+            Text(text)
+                .font(.caption)
+                .foregroundColor(.white)
+                .padding(5)
+                .background(.black)
+        }
+    }
+}
+
+extension View {
+    func watermarked(with text: String) -> some View {
+        modifier(Watermark(text: text))
+    }
+}
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
 //        ContentView()
-        ContentView2()
+//        ContentView2()
+//        ContentView3()
+        ContentView4()
     }
 }
